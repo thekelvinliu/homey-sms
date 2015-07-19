769c2492d978ed4e06dd94a58d0d4bcfc75c166f
thekelvinliu <kelvin@thekelvinliu.com>
2015-07-19 00:49:26 -0400

    started switch case for keywords

--- app.js
+++ app.js
@@ -1,3 +1,9 @@
+// TWILIO
+var account_sid = "ACd882274f43e1f2fffb2f26e4348ea273";
+var auth_token = "7db2af93aa137cfeb9eff5224fa18ec8";
+var twilio = require('twilio');
+var client = new twilio.RestClient(account_sid, auth_token);
+
 // EXPRESS
 var express = require('express');
 var app = express();
@@ -31,13 +37,27 @@ app.get('/test', function(req, res) {
 });
 
 app.get('/twiml', function(req, res) {
+  var resp = new twilio.TwimlResponse();
   var data = req.query;
+  console.log(data);
   if (!data.hasOwnProperty('Body')) {
     console.log("Not a text.");
     res.send("404");
   } else {
     var body = data.Body;
-    console.log(body);
-    res.sendFile(__dirname + '/views/r.xml');
+    switch (body) {
+      case "SHELTER":
+        var msg = "Where are you? Send me your nearest crosst street (e.g. 5th Ave and W 20th St), and I'll give you info on the shelter closest to you!";
+        resp.message(msg);
+        break;
+      case "FOOD":
+        var msg = "Where are you? Send me your nearest crosst street (e.g. 5th Ave and W 20th St), and I'll give you info on the soup kitchen closest to you!";
+        resp.message(msg);
+        break;
+      default:
+        var msg = "Huh, not sure what you're trying to do? Text SHELTER for info on shelters and FOOD for info on soup kitchens near you!";
+        resp.message(msg);
+    }
+    res.send(resp.toString());
   }
 });
